862db4c705b9469c4b86b33a878446efdac6dc77
Pavel Bezliapovich <pavel.bezliapovich@sbb.ch>
2025-10-30 15:55:23 +0100

    feat: add env-based max upload size for Pandoc container
    Refs: #75

--- app/PandocController.py
+++ app/PandocController.py
@@ -93,7 +93,7 @@ app = FastAPI(
 
 
 # Validate PANDOC_SERVICE_DATA_LIMIT environment variable
-def get_validated_data_limit():
+def get_validated_data_limit() -> int:
     default_limit_mb = 500
     max_limit_mb = 10000
     env_value = os.environ.get("PANDOC_SERVICE_DATA_LIMIT", str(default_limit_mb))
