d551c63d3eac870f0786752958d560498940fd39
Pavel Bezliapovich <pavel.bezliapovich@sbb.ch>
2026-01-21 10:10:44 +0100

    fix: increase multipart form data size limit
    Refs: #119

--- app/PandocController.py
+++ app/PandocController.py
@@ -455,7 +455,7 @@ async def convert_pptx_with_ref(  # noqa: PLR0913
 ) -> Response:
     temp_template_filename = None
     try:
-        form = await request.form(max_part_size=data_limit) # NOSONAR type: ignore[call-arg]
+        form = await request.form(max_part_size=data_limit)
         source_content = form.get("source")
         source = await get_docx_source_data(source_content, encoding)
         if not source:
